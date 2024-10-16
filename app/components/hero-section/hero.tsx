be61e6a684e7eacc19045abe30023859b2c3d135
ar813 <rafaykhan007852@gmail.com>
2024-10-16 19:48:01 +0500

    initialize

--- app/components/hero-section/hero.tsx
+++ app/components/hero-section/hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
   return (
       <div className={css.hero}>
         <h1>Find the perfect <span className={css.span}>Freelance</span> services for your business</h1>
-        <p>We're a team of freelancers who specialize in helping businesses grow and succeed online.</p>
+        <p>Were a team of freelancers who specialize in helping businesses grow and succeed online.</p>
         <div className={css.buttons}>
           <button className={css.heroButton}>Get Started</button>
           <button className={css.heroButton}>Learn More</button>
