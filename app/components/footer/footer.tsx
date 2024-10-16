5acf301a841df3d590bd79bf36b170b011b7113c
ar813 <rafaykhan007852@gmail.com>
2024-10-16 19:37:10 +0500

    git init

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
                 <p>Phone: 1234567890</p>
                 <p>Email: info@example.com</p>
                 <p>Address: 123 Main Street, Anytown, CA 12345</p>
-                <p>&copy; All right reserved (2024)</p>
+                <p>All right reserved (2024)</p>
             </footer>
         </div>
     )
